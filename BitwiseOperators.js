44fac035223cd250d8c246b7010726424f1426c6
sm-dotcom <78915351+sm-dotcom@users.noreply.github.com>
2021-10-06 15:47:04 +0500

    Add files via upload

---
+++ BitwiseOperators.js
@@ -0,0 +1,12 @@
+let A = 1
+let B = 2
+
+console.log(A & B)
+console.log(A | B)
+
+let y = 3^5
+console.log(~y)
+
+
+let x = 00001010
+console.log(~x)
---
+++ if_else.js
@@ -0,0 +1,15 @@
+let age = 18
+
+if (age > 18) {
+    console.log('congratulations you can get a drivers license')
+}
+
+else if (age == 18){
+    console.log('congratulations you are eligible for a license drivers license')
+}
+
+else{
+    console.log('oops! sorry')
+}
+
+
