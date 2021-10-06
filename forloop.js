65ebf460bdb3350ca84b2771ab3025b8e542f6a7
sm-dotcom <78915351+sm-dotcom@users.noreply.github.com>
2021-10-06 18:47:25 +0500

    Add files via upload

---
+++ forloop.js
@@ -0,0 +1,22 @@
+let x;
+
+for (let i = 0; i < 5; i++)
+{
+    console.log('value of i is : ', i)
+}
+
+let fruits = ['Apple', 'Banana', 'Grapes']
+// let x
+for (x in fruits){
+    console.log (fruits[x])
+}
+
+for (x of fruits){
+    console.log(x)
+}
+
+let cars = ['bmw','ciaz','cairo', 'mehran']
+
+for (b of cars){
+    console.log(b)
+}
